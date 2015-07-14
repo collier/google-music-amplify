0bdcd208bcfdd4749b585a07f019aef29aa19e33
collier <dj@getfoodio.com>
2015-07-14 16:48:39 -0400

    Updates manifest and background.js

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,11 +1,56 @@
 'use strict';
 
-chrome.runtime.onInstalled.addListener(function (details) {
-  console.log('previousVersion', details.previousVersion);
-});
+// chrome.runtime.onInstalled.addListener(function (details) {
+//  // Replace all rules ...
+//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+//     // With a new rule ...
+//     chrome.declarativeContent.onPageChanged.addRules([
+//       {
+//         // That fires when a page's URL contains a 'g' ...
+//         conditions: [
+//           new chrome.declarativeContent.PageStateMatcher({
+//             pageUrl: {
+//               urlContains: 'google',
+//               schemes: ['https','http']
+//             }
+//           })
+//         ],
+//         // And shows the extension's page action.
+//         actions: [ new chrome.declarativeContent.ShowPageAction() ]
+//       }
+//     ]);
+//   });
+// });
+
+// chrome.tabs.onUpdated.addListener(function (tabId) {
+//   chrome.pageAction.show(tabId);
+// });
+
+// chrome.tabs.onUpdated.addListener(function (tabId) {
+  // chrome.pageAction.show(tabId);
+// });
+
 
 chrome.tabs.onUpdated.addListener(function (tabId) {
-  chrome.pageAction.show(tabId);
+ // Replace all rules ...
+  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+    // With a new rule ...
+    chrome.declarativeContent.onPageChanged.addRules([
+      {
+        // That fires when a page's URL contains a 'g' ...
+        conditions: [
+          new chrome.declarativeContent.PageStateMatcher({
+            pageUrl: {
+              urlContains: 'google',
+              schemes: ['https','http']
+            }
+          })
+        ],
+        // And shows the extension's page action.
+        actions: [ new chrome.pageAction.show(tabId) ]
+      }
+    ]);
+  });
 });
 
 console.log('\'Allo \'Allo! Event Page for Page Action');
