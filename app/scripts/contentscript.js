dd29e85fe12844c27c491aef0d640d2a238906c9
collier <dj@getfoodio.com>
2015-07-15 13:22:00 -0400

    Fix adding multiple buttons and dynamically inject styles

--- app/scripts/contentscript.js
+++ app/scripts/contentscript.js
@@ -2,19 +2,27 @@
 
 $(function() {
 
-  $('#\\:i').click(function(){
+
+  //Dynamically append the inject.css stylsheet into the head,
+  //so that styles marked with !imporant can still be overridden
+  var link = document.createElement("link");
+  link.href = chrome.extension.getURL("styles/inject.css");
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  $("head").append(link);
+
+
+  //Add event listener for the "Edit Info" drop down option. When clicked, wait
+  //for the Edit song info popup to load, then append the "Find Metadata"
+  //button to form, if it hasn't already been added.
+  $('#\\:i').click(function() {
     setTimeout(function() {
-      $('.simple-dialog.edit-dialog')
-        .find('.simple-dialog-buttons')
-        .append('<button name="search-metadata">Find Metadata</button>');
-    }, 300);
+      var buttons = $('.simple-dialog.edit-dialog')
+                      .find('.simple-dialog-buttons');
+      if(buttons.find('button[name="search-metadata"]').length === 0) {
+        buttons.append('<button name="search-metadata">Find Metadata</button>');
+      }
+    }, 500);
   });
-  // console.log($('#\\:i'));
 
 });
-
-// document.getElementById(':i').addEventListener('click', function(){
-//   document.getElementsByClassName('simple-dialog edit-dialog')[0];
-// });
-
-console.log('\'Allo \'Allo! Content script');
