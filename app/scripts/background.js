6c4f26fb4394b85e23db8781808d92651ea55516
collier <dj@getfoodio.com>
2015-07-15 00:22:07 -0400

    Adds find metadata button to edit song popup

--- app/scripts/background.js
+++ app/scripts/background.js
@@ -1,53 +1,22 @@
 'use strict';
 
-// chrome.runtime.onInstalled.addListener(function (details) {
-//  // Replace all rules ...
-//   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-//     // With a new rule ...
-//     chrome.declarativeContent.onPageChanged.addRules([
-//       {
-//         // That fires when a page's URL contains a 'g' ...
-//         conditions: [
-//           new chrome.declarativeContent.PageStateMatcher({
-//             pageUrl: {
-//               urlContains: 'google',
-//               schemes: ['https','http']
-//             }
-//           })
-//         ],
-//         // And shows the extension's page action.
-//         actions: [ new chrome.declarativeContent.ShowPageAction() ]
-//       }
-//     ]);
-//   });
-// });
-
-// chrome.tabs.onUpdated.addListener(function (tabId) {
-//   chrome.pageAction.show(tabId);
-// });
-
-// chrome.tabs.onUpdated.addListener(function (tabId) {
-  // chrome.pageAction.show(tabId);
-// });
-
-
-chrome.tabs.onUpdated.addListener(function (tabId) {
+chrome.runtime.onInstalled.addListener(function (details) {
  // Replace all rules ...
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     // With a new rule ...
     chrome.declarativeContent.onPageChanged.addRules([
       {
-        // That fires when a page's URL contains a 'g' ...
+        // Fires when Google Music loads
         conditions: [
           new chrome.declarativeContent.PageStateMatcher({
             pageUrl: {
-              urlContains: 'google',
+              urlContains: 'play.google.com/music',
               schemes: ['https','http']
             }
           })
         ],
         // And shows the extension's page action.
-        actions: [ new chrome.pageAction.show(tabId) ]
+        actions: [ new chrome.declarativeContent.ShowPageAction() ]
       }
     ]);
   });
