249db3c1a5f8ae135f1bf2b73ea01976103670bb
collier <dj@getfoodio.com>
2015-07-25 12:41:55 -0400

    Switches material theme to light-blue. Switches back to close svg

--- app/modules/metadata/metadata.js
+++ app/modules/metadata/metadata.js
@@ -4,5 +4,9 @@ var ngMaterial  = require('angular-material'),
     Spotify     = require('angular-spotify');
 
 module.exports = angular.module('GMA_Metadata', ['ngMaterial', 'spotify'])
+  .config(function($mdThemingProvider){
+    $mdThemingProvider.theme('default')
+      .primaryPalette('light-blue')
+  })
   .controller('FindBtnCtrl', require('./find-btn.js'))
   .controller('DialogCtrl', require('./dialog.js'));
